ac0e0253c698b720901661587ab367c43456336b
Rodrigo <rodrigo.estudante@email.com>
2025-07-02 18:35:23 -0300

    Atualiza backend: Java 17 + Spring Boot 3.2.0 + Jakarta Persistence

--- movies-api/src/main/java/com/apispring/moviesapi/entities/Favorite.java
+++ movies-api/src/main/java/com/apispring/moviesapi/entities/Favorite.java
@@ -1,6 +1,6 @@
 package com.apispring.moviesapi.entities;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
